f450eda1febbe7735eed4bc97a5ea81859d36519
Isacco Rossi <ir.isacco.rossi@gmail.com>
2020-11-09 11:03:36 +0100

    DEV | Add NoData components

---
+++ src/components/NoData.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-3 rounded flex flex-row items-center justify-center">
+    <i class="hi-inbox text-gray-400 text-2xl mr-3" />
+    <slot name="message">
+      <p class="text-center text-gray-400 font-semibold">Sembra che non ci siano ancora dati da analizzare.</p>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
