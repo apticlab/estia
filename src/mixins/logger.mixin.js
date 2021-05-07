b937fd87c854e40a6c6fa7c9cf6cf3e82b1d5543
isareds <ir.isacco.rossi@gmail.com>
2021-05-07 18:01:27 +0200

    dev | if is in production logger will no longer print message

--- src/mixins/logger.mixin.js
+++ src/mixins/logger.mixin.js
@@ -2,10 +2,12 @@ export default {
   data() {
     return {};
   },
-  mounted() {},
+  mounted() { },
   methods: {
     log(...logs) {
-      console.log(`[${this.$options.name}]`, ...logs);
+      if (process.env.NODE_ENV != 'production') {
+        console.log(`[${this.$options.name}]`, ...logs);
+      }
     },
   },
   computed: {},
