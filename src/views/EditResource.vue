912bc61d61ede14d3fd65999efeadb67fcc121dd
Isacco Rossi <ir.isacco.rossi@gmail.com>
2021-09-17 14:57:52 +0200

    dev | in EditResource add props to hide actions

--- src/views/EditResource.vue
+++ src/views/EditResource.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="flex flex-col items-center overflow-hidden rounded align-center">
-    <loading
-      v-if="loading"
-      class="w-full my-16"
-    />
-    <div
-      v-if="!loading"
-      class="w-full mx-auto max-w-screen-xl"
-    >
+    <loading v-if="loading" class="w-full my-16" />
+    <div v-if="!loading" class="w-full mx-auto max-w-screen-xl">
       <div class="">
         <awesome-form
           v-if="!loading"
@@ -18,7 +12,7 @@
           :headers="form_fields"
           :validate="true"
           :layout="layout"
-          @valid="_valid => (valid = _valid)"
+          @valid="(_valid) => (valid = _valid)"
           @change="updateResource"
         />
         <div
@@ -37,20 +31,24 @@
             </li>
           </ol>
         </div>
-        <div class="flex flex-row items-center justify-end w-full py-5">
+        <div class="flex flex-row items-center justify-end w-full py-5" v-if='!hideActions'>
           <div class="flex flex-row items-baseline mr-auto">
             <button
               v-for="action in visibleActions"
               :key="action.label"
               :class="'bg-' + action.color"
-              class="px-4 ml-3 ml-auto text-white rounded-none outline-none focus:outline-none"
+              class="
+                px-4
+                ml-3 ml-auto
+                text-white
+                rounded-none
+                outline-none
+                focus:outline-none
+              "
               @click="act(action)"
             >
               <span class="flex flex-row justify-center">
-                <i
-                  :class="action.icon"
-                  class="mt-1 mr-2 text-md"
-                />
+                <i :class="action.icon" class="mt-1 mr-2 text-md" />
                 <span>{{ action.label }}</span>
               </span>
             </button>
@@ -76,10 +74,7 @@
             <p class="text-red-600 text-weigth-600">
               {{ error }}
             </p>
-            <button
-              class="ml-auto btn btn-primary"
-              @click="retry()"
-            >
+            <button class="ml-auto btn btn-primary" @click="retry()">
               <i class="mr-2 fas fa-redo-alt" />
               <span>Riprova</span>
             </button>
@@ -91,36 +86,40 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 const rest_resources = {
-  profile: 'users'
-}
+  profile: "users",
+};
 
 export default {
-  name: 'ResourceEdit',
+  name: "ResourceEdit",
   props: {
     component: { required: false, default: false },
     propResourceName: {
       required: false,
       type: String,
-      default: null
+      default: null,
     },
     propResourceId: {
       required: false,
       type: Number,
-      default: null
+      default: null,
     },
     layout: {
       required: false,
-      default: 'vertical'
+      default: "vertical",
     },
     debug: {
       required: false,
-      default: false
-    }
+      default: false,
+    },
+    hideActions: {
+      required: false,
+      default: true,
+    },
   },
-  data () {
+  data() {
     return {
       resourceErrors: null,
       changedResource: {},
@@ -131,42 +130,42 @@ export default {
       resource_name: null,
       actions: [],
       is_edit: false,
-      valid: false
-    }
+      valid: false,
+    };
   },
-  async mounted () {
-    this.loading = true
+  async mounted() {
+    this.loading = true;
 
-    this.resource_id = this.propResourceId || this.$route.params.id
+    this.resource_id = this.propResourceId || this.$route.params.id;
     this.resource_name =
       this.propResourceName ||
       this.$route.params.resource ||
-      this.$route.meta.resource
+      this.$route.meta.resource;
 
     this.resource_rest_name =
-      rest_resources[this.resource_name] || this.resource_name
+      rest_resources[this.resource_name] || this.resource_name;
 
-    this.actions = this.resources[this.resource_name].actions || []
+    this.actions = this.resources[this.resource_name].actions || [];
 
     if (this.debug) {
-      this.log('ResourceName: ' + this.resource_name)
-      this.log('ResourceId: ' + this.resource_id)
+      this.log("ResourceName: " + this.resource_name);
+      this.log("ResourceId: " + this.resource_id);
     }
 
     if (this.resource_id) {
-      this.is_edit = true
+      this.is_edit = true;
       this.resource = await this.$api.get(
         this.resource_rest_name,
         this.resource_id
-      )
+      );
     }
 
-    this.loading = false
+    this.loading = false;
   },
   methods: {
-    async saveResource () {
-      this.resourceErrors = null
-      let resource_name = this.resource_rest_name || this.resource_name
+    async saveResource() {
+      this.resourceErrors = null;
+      let resource_name = this.resource_rest_name || this.resource_name;
 
       try {
         if (this.is_edit) {
@@ -174,114 +173,114 @@ export default {
             resource_name,
             this.resource_id,
             this.changedResource
-          )
+          );
         } else {
-          let resource = _.clone(this.changedResource)
-          await this.$api.create(resource_name, resource)
+          let resource = _.clone(this.changedResource);
+          await this.$api.create(resource_name, resource);
           if (this.p_resource) {
-            this.$emit('save', true)
-            return
+            this.$emit("save", true);
+            return;
           }
         }
 
-        this.$router.back()
+        this.$router.back();
       } catch (err) {
-        console.log(err)
+        console.log(err);
 
         if (err.errors) {
-          this.resourceErrors = []
+          this.resourceErrors = [];
 
-          Object.keys(err.errors).map(e => {
-            console.log(err.errors[e])
-            this.resourceErrors = this.resourceErrors.concat(err.errors[e])
-          })
+          Object.keys(err.errors).map((e) => {
+            console.log(err.errors[e]);
+            this.resourceErrors = this.resourceErrors.concat(err.errors[e]);
+          });
 
-          return
+          return;
         }
 
         switch (err.message) {
-          case 'The given data was invalid.':
-            this.error = 'Alcuni campi non sono validi.'
-            break
+          case "The given data was invalid.":
+            this.error = "Alcuni campi non sono validi.";
+            break;
 
           default:
-            this.error = "Ops! C'è stato un errore."
+            this.error = "Ops! C'è stato un errore.";
         }
       }
     },
-    async retry () {
-      this.error = null
+    async retry() {
+      this.error = null;
 
-      await this.saveResource()
+      await this.saveResource();
     },
-    back () {
+    back() {
       if (this.p_resource) {
-        this.$emit('close')
-        return
+        this.$emit("close");
+        return;
       }
 
-      this.$router.back()
+      this.$router.back();
     },
-    updateResource (newResource) {
+    updateResource(newResource) {
       if (this.debug) {
-        this.log('NewResource')
-        this.log(newResource)
+        this.log("NewResource");
+        this.log(newResource);
       }
 
       this.changedResource = newResource;
     },
-    act (action) {
+    act(action) {
       if (this[action.callback]) {
-        this[action.callback]()
+        this[action.callback]();
       }
     },
-    delete () {
-      if (confirm('Vuoi davvero eliminare questa risorsa?')) {
-        this.isLoading = true
-        this.$api.delete(this.resource_name, this.resource_id)
-        this.isLoading = false
+    delete() {
+      if (confirm("Vuoi davvero eliminare questa risorsa?")) {
+        this.isLoading = true;
+        this.$api.delete(this.resource_name, this.resource_id);
+        this.isLoading = false;
 
-        this.$router.push('../list')
+        this.$router.push("../list");
       }
-    }
+    },
   },
   computed: {
-    ...mapState('user', {
-      user: state => state.user
+    ...mapState("user", {
+      user: (state) => state.user,
     }),
-    form_fields () {
-      let fields = this.resources[this.resource_name].fields || []
+    form_fields() {
+      let fields = this.resources[this.resource_name].fields || [];
 
-      return fields.filter(field => {
+      return fields.filter((field) => {
         if (!field.scopes) {
-          return true
+          return true;
         }
 
-        return field.scopes.includes('edit')
-      })
+        return field.scopes.includes("edit");
+      });
     },
-    button_label () {
-      return this.is_edit ? 'Conferma' : 'Conferma'
+    button_label() {
+      return this.is_edit ? "Conferma" : "Conferma";
     },
-    action_name () {
-      return this.is_edit ? 'Modifica' : 'Nuova'
+    action_name() {
+      return this.is_edit ? "Modifica" : "Nuova";
     },
-    resourceInfo () {
-      return this.resources[this.resource_name].info || {}
+    resourceInfo() {
+      return this.resources[this.resource_name].info || {};
     },
-    options () {
-      return this.resourceInfo.singular || 'Risorsa'
+    options() {
+      return this.resourceInfo.singular || "Risorsa";
     },
-    visibleActions () {
-      return this.actions.filter(action => {
+    visibleActions() {
+      return this.actions.filter((action) => {
         return (
           !action.scopes ||
-          action.scopes.includes(this.is_edit ? 'edit' : 'create')
-        )
-      })
-    }
-  }
-}
+          action.scopes.includes(this.is_edit ? "edit" : "create")
+        );
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
