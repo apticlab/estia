30809b8bf27540df50e170d9aa1cc9e564e52650
Isacco Rossi <ir.isacco.rossi@gmail.com>
2021-10-22 15:45:13 +0200

    dev | add new logic to understand if app is used in a mobile phone through navigator api

--- src/mixins/mobile.mixin.js
+++ src/mixins/mobile.mixin.js
@@ -6,6 +6,13 @@ export default {
   methods: {},
   computed: {
     is_mobile() {
+      if(navigator) {
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+          return true;
+        } else {
+          return this.windowWidth < 640;
+        }
+      }
       return this.windowWidth < 640;
     },
   },
