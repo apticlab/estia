0c7a6e96873e50fed8ce998dd99a950321011745
Isacco Rossi <ir.isacco.rossi@gmail.com>
2022-10-03 16:30:04 +0200

    dev | add screen param that return the screen size name using tailwind nomenclature for breakpoints

--- src/mixins/mobile.mixin.js
+++ src/mixins/mobile.mixin.js
@@ -2,12 +2,12 @@ export default {
   data() {
     return {};
   },
-  mounted() {},
+  mounted() { },
   methods: {},
   computed: {
     is_mobile() {
-      if(navigator) {
-        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      if (navigator) {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
           return true;
         } else {
           return this.windowWidth < 640;
@@ -15,5 +15,21 @@ export default {
       }
       return this.windowWidth < 640;
     },
+    screen() {
+      let dimension = 'mobile';
+
+      if (this.windowWidth >= 640) {
+        dimension = 'sm';
+      } else if (this.windowWidth >= 768) {
+        dimension = 'md';
+      } else if (this.windowWidth >= 1024) {
+        dimension = 'lg';
+      } else if (this.windowWidth >= 1280) {
+        dimension = 'xl';
+      } else if (this.windowWidth >= 1536) {
+        dimension = '2xl';
+      }
+      return dimension;
+    }
   },
 };
