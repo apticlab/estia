33e25badf3779544cde7ab35881f8a01baeb7ddd
Isacco Rossi <ir.isacco.rossi@gmail.com>
2022-10-03 13:02:46 +0200

    dev | remove the example folder

--- examples/simple-app/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import LoginPage from '@/components/LoginPage.vue';
-import Main from '@/components/Main.vue';
-
-const baseComponents = {
-    login: LoginPage,
-    main: Main,
-  };
-
-export default baseComponents;
