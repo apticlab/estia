6e4d7ca68c677cd10ec7146c93cae5ece665e006
Isacco Rossi <ir.isacco.rossi@gmail.com>
2022-10-10 09:54:38 +0200

    dev | format code

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,28 +1,43 @@
 <template>
-  <div>
-    <div
-      @click="focusInput()"
-      class="px-2 h-11 text-sm border border-gray-300 rounded flex flex-row items-center bg-white cursor-text"
+  <div
+    @click="focusInput()"
+    class="
+      px-2
+      h-11
+      text-sm
+      border border-gray-300
+      rounded
+      flex flex-row
+      items-center
+      bg-white
+      cursor-text
+    "
+  >
+    <icon name="search" color="text-gray-300" size="m" class="mr-3"></icon>
+    <input
+      ref="input"
+      class="
+        m-0
+        p-0
+        bg-transparent
+        border-none
+        focus:outline-none
+        active:outline-none
+      "
+      style="height: initial"
+      type="text"
+      :placeholder="placeholder"
+      @input="onInput"
+      v-on:keyup.enter="onEnter"
+      v-model="inputValue"
+    />
+    <span
+      class="text-xs text-gray-400 cursor-pointer"
+      v-show="inputValue"
+      @click="clearInput()"
     >
-      <icon name="search" color="text-gray-300" size="m" class="mr-3"></icon>
-      <input
-        ref="input"
-        class="m-0 p-0 bg-transparent border-none focus:outline-none active:outline-none"
-        style="height: initial"
-        type="text"
-        :placeholder="placeholder"
-        @input="onInput"
-        v-on:keyup.enter="onEnter"
-        v-model="inputValue"
-      />
-      <span
-        class="text-xs text-gray-400 cursor-pointer"
-        v-show="inputValue"
-        @click="clearInput()"
-      >
-        Cancella
-      </span>
-    </div>
+      Cancella
+    </span>
   </div>
 </template>
 <script>
@@ -33,25 +48,25 @@ export default {
   props: {
     value: {
       type: String,
-      required: false
+      required: false,
     },
     placeholder: {
       type: String,
       required: false,
-      default: "Cerca"
+      default: "Cerca",
     },
     mode: {
       type: String,
       required: false,
       default: "enter",
-      validator: mode => {
+      validator: (mode) => {
         return ["enter", "debounce"].includes(mode);
-      }
-    }
+      },
+    },
   },
   data() {
     return {
-      inputValue: null
+      inputValue: null,
     };
   },
   async mounted() {
@@ -68,7 +83,7 @@ export default {
         this.$emit("input", this.inputValue);
       }
     },
-    debounceInput: _.debounce(function() {
+    debounceInput: _.debounce(function () {
       this.$emit("input", this.inputValue);
     }, 350),
     clearInput() {
@@ -77,8 +92,8 @@ export default {
     },
     focusInput() {
       this.$refs.input.focus();
-    }
+    },
   },
-  computed: {}
+  computed: {},
 };
 </script>
