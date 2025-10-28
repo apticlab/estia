1526de0f6d5b0d923845dcd0d361f9ca3fd85528
Isacco Rossi <ir.isacco.rossi@gmail.com>
2025-10-28 09:00:17 +0100

    using phosporicons instead of manual svg

--- src/components/DialogModal.vue
+++ src/components/DialogModal.vue
@@ -1,59 +1,21 @@
 <template>
   <transition name="fade">
     <div v-if="visible" class="fixed inset-0 z-50">
-      <icon
-        name="x"
-        size="l"
-        class="absolute top-[10px] right-[10px] text-white z-[60] cursor-pointer"
-        @click="hide()"
-      />
-      <div
-        :class="$theme.backdropBgColor || 'opacity-50 bg-gray-dark'"
-        class="absolute inset-0"
-        @click="hide()"
-      />
-      <div
-        ref="backdrop"
-        class="absolute inset-0 flex flex-col items-center justify-center"
-        @click="handleBackdropClick($event)"
-      >
+      <PhX class="absolute top-[10px] right-[10px] text-white z-[60] cursor-pointer h-12 w-12" @click="hide()" />
+      <div :class="$theme.backdropBgColor || 'opacity-50 bg-gray-dark'" class="absolute inset-0" @click="hide()" />
+      <div ref="backdrop" class="absolute inset-0 flex flex-col items-center justify-center"
+        @click="handleBackdropClick($event)">
         <div v-if="type == 'change_password'">
           <change-password :params="params" @done="confirm" />
         </div>
-        <template v-else-if="type == 'checkin'">
-          <check-in-modal
-            :params="params"
-            :class="is_mobile ? 'h-full w-full' : ''"
-            @done="confirm"
-          />
-        </template>
-        <template v-else-if="type == 'checkout'">
-          <check-in-modal
-            :params="params"
-            :class="is_mobile ? 'h-full w-full' : ''"
-            @done="confirm"
-          />
-        </template>
-        <template v-else-if="type == 'import-csv'">
-          <import-csv-modal :params="params" @done="confirm" />
-        </template>
         <template v-else-if="$modalWidgets[type]">
-          <component
-            :is="$modalWidgets[type]"
-            :params="params"
-            @done="confirm"
-            @hide="hide"
-          />
+          <component :is="$modalWidgets[type]" :params="params" @done="confirm" @hide="hide" />
         </template>
         <template v-else>
-          <div
-            :class="
-              is_mobile
-                ? 'h-full w-full'
-                : 'h-auto my-10 overflow-y-auto sm:w-8/12'
-            "
-            class="flex flex-col p-5 bg-white rounded-none shadow-2xl sm:rounded-lg"
-          >
+          <div :class="is_mobile
+              ? 'h-full w-full'
+              : 'h-auto my-10 overflow-y-auto sm:w-8/12'
+            " class="flex flex-col p-5 bg-white rounded-none shadow-2xl sm:rounded-lg">
             <div class="flex-grow p-4">
               <div class="flex flex-col items-baseline mb-4">
                 <div class="flex-cont-col">
@@ -62,32 +24,17 @@
                   </h2>
                   <p>{{ params.text }}</p>
                 </div>
-                <resource-edit
-                  v-if="type == 'resource-edit'"
-                  :prop-resource-name="params.resource"
-                  :prop-resource-id="params.resourceId"
-                  :prop-resource-value="params.values"
-                  :event="true"
-                  class="w-full h-auto"
-                  @save="confirm()"
-                  @close="hide()"
-                />
+                <resource-edit v-if="type == 'resource-edit'" :prop-resource-name="params.resource"
+                  :prop-resource-id="params.resourceId" :prop-resource-value="params.values" :event="true"
+                  class="w-full h-auto" @save="confirm()" @close="hide()" />
               </div>
             </div>
-            <div
-              v-if="!type"
-              class="flex flex-row items-center justify-between px-4 py-3 bg-gray-100 sm:justify-end"
-            >
-              <button
-                class="px-3 py-2 ml-0 mr-3 text-blue-600 sm:ml-auto no-outline"
-                @click="confirm(false)"
-              >
+            <div v-if="!type" class="flex flex-row items-center justify-between px-4 py-3 bg-gray-100 sm:justify-end">
+              <button class="px-3 py-2 ml-0 mr-3 text-blue-600 sm:ml-auto no-outline" @click="confirm(false)">
                 {{ params.cancelText || defaultCancelText }}
               </button>
-              <button
-                class="px-3 py-2 text-white rounded-none bg-blue-600 tx-bold focus:outline-none"
-                @click="confirm(true)"
-              >
+              <button class="px-3 py-2 text-white rounded-none bg-blue-600 tx-bold focus:outline-none"
+                @click="confirm(true)">
                 {{ params.confirmText || defaultConfirmText }}
               </button>
             </div>
@@ -101,11 +48,13 @@
 <script>
 import Dialog from "../plugins/dialog";
 import ChangePassword from "@/components/ChangePassword.vue";
+import { PhX } from '@phosphor-icons/vue';
 
 export default {
   name: "DialogModal",
   components: {
     "change-password": ChangePassword,
+    PhX,
   },
   data() {
     return {
@@ -193,6 +142,7 @@ export default {
   border-radius: 2px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 }
+
 .modal-buttons {
   position: absolute;
   bottom: 0;
@@ -200,6 +150,7 @@ export default {
   right: 0;
   display: flex;
 }
+
 .modal-button {
   flex-grow: 1;
 }
