0a4b045d514b4b29d661ae6782368319570f6d93
Isacco Rossi <ir.isacco.rossi@gmail.com>
2025-11-15 18:59:08 +0100

    Refactor filters to export individual functions for Vue 3 compatibility

--- src/filters/filters.js
+++ src/filters/filters.js
@@ -2,123 +2,145 @@ import moment from "moment";
 import i18n from "../i18n";
 import { helpers } from "../utils/helpers";
 
-export default {
-  leftpad: function(value, number = 4) {
-    return value.toString().padStart(number, "0");
-  },
-  count: function(value) {
-    if (value != undefined && value != null) {
-      return value.length;
-    }
-    return 0;
-  },
-  uppercase: function(value) {
-    if (value != undefined && value != null) {
-      return value.toUpperCase();
-    }
-  },
-  capitalize: function(value) {
-    if (value != undefined && value != null && value != "") {
-      return value[0].toUpperCase() + value.substring(1, value.length);
-    }
-  },
-  date: function(value, format) {
-    if (!value) return "";
+export function leftpad(value, number = 4) {
+  return value != null ? value.toString().padStart(number, "0") : "";
+}
 
-    var date = moment(value);
-
-    if (!date.isValid()) return "";
-    return format ? date.format(format) : date;
-  },
-  number: function(value) {
-    let number;
-    if (value < "999") {
-      return value;
-    }
-    if (value < "999999") {
-      number = value / 1000;
-      return number.toFixed(1).replace(".", ",") + " K";
-    }
-    if (value < 999999999) {
-      number = value / 1000000;
-      return number.toFixed(1).replace(".", ",") + " M";
-    }
-  },
-  round: function(value, digits = 2) {
-    // Round only if it is float
-    if (value % 1 === 0) {
-      return value;
-    }
-
-    // TODO: round locale wise
-    return parseFloat(value)
-      .toFixed(digits)
-      .replace(".", ",");
-  },
-  percentage: function(value, digits = 1) {
-    let percValue = value * 100;
-
-    // TODO: round locale wise
-    let parsePercValue = parseFloat(percValue)
-      .toFixed(digits)
-      .replace(".", ",");
-
-    return parsePercValue + "%";
-  },
-  time_ago: function(value) {
-    if (!value) return "";
-    return moment(value)
-      .locale("it")
-      .fromNow();
-  },
-  on_empty(value, message) {
-    if (!value) return message;
-    return value;
-  },
-  translate(key) {
-    const locale = i18n.locale;
-    const messages = i18n.messages[locale];
-    return helpers.deepPick(messages, key) || key;
-  },
-  week_date(week, year = moment().year()) {
-    if (!week) return "";
-    const week_as_string = moment()
-      .day("Sunday")
-      .year(year)
-      .week(week);
-    return {
-      start: week_as_string.startOf("week").format("L"),
-      end: week_as_string.endOf("week").format("L")
-    };
-  },
-  prettify(value, lang) {
-    switch (lang) {
-      case "json":
-        return JSON.stringify(value, null, "\t");
-      default:
-        return value;
-    }
-  },
-  json(value) {
-    /* value = value
-      .replace(/\n/g, '\\n')
-      .replace(/\r/g, '\\r')
-      .replace(/\t/g, '\\t'); */
-    try {
-      return JSON.parse(value);
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
-  },
-  truncate(text, length, clamp = '...') {
-    if(!length) {
-      return text;
-    }
-
-    var node = document.createElement('div');
-    node.innerHTML = text;
-    var content = node.textContent;
-    return content.length > length ? content.slice(0, length) + clamp : content;
+export function count(value) {
+  if (value != undefined && value != null) {
+    return value.length;
   }
+  return 0;
+}
+
+export function uppercase(value) {
+  if (value != undefined && value != null) {
+    return value.toUpperCase();
+  }
+  return value;
+}
+
+export function capitalize(value) {
+  if (value != undefined && value != null && value !== "") {
+    return value[0].toUpperCase() + value.substring(1, value.length);
+  }
+  return value;
+}
+
+export function date(value, format) {
+  if (!value) return "";
+
+  const momentValue = moment(value);
+  if (!momentValue.isValid()) return "";
+
+  return format ? momentValue.format(format) : momentValue;
+}
+
+export function number(value) {
+  if (value < "999") {
+    return value;
+  }
+  if (value < "999999") {
+    const thousand = value / 1000;
+    return thousand.toFixed(1).replace(".", ",") + " K";
+  }
+  if (value < 999999999) {
+    const million = value / 1000000;
+    return million.toFixed(1).replace(".", ",") + " M";
+  }
+  return value;
+}
+
+export function round(value, digits = 2) {
+  if (value % 1 === 0) {
+    return value;
+  }
+
+  return parseFloat(value)
+    .toFixed(digits)
+    .replace(".", ",");
+}
+
+export function percentage(value, digits = 1) {
+  const percValue = value * 100;
+
+  return (
+    parseFloat(percValue)
+      .toFixed(digits)
+      .replace(".", ",") + "%"
+  );
+}
+
+export function time_ago(value) {
+  if (!value) return "";
+  return moment(value).locale("it").fromNow();
+}
+
+export function on_empty(value, message) {
+  if (!value) return message;
+  return value;
+}
+
+export function translate(key) {
+  const locale = i18n.locale;
+  const messages = i18n.messages[locale];
+  return helpers.deepPick(messages, key) || key;
+}
+
+export function week_date(week, year = moment().year()) {
+  if (!week) return "";
+  const weekAsMoment = moment().day("Sunday").year(year).week(week);
+  return {
+    start: weekAsMoment.startOf("week").format("L"),
+    end: weekAsMoment.endOf("week").format("L"),
+  };
+}
+
+export function prettify(value, lang) {
+  switch (lang) {
+    case "json":
+      return JSON.stringify(value, null, "\t");
+    default:
+      return value;
+  }
+}
+
+export function json(value) {
+  try {
+    return JSON.parse(value);
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export function truncate(text, length, clamp = "...") {
+  if (!length || !text) {
+    return text;
+  }
+
+  const node = document.createElement("div");
+  node.innerHTML = text;
+  const content = node.textContent || "";
+  return content.length > length ? content.slice(0, length) + clamp : content;
+}
+
+const filters = {
+  leftpad,
+  count,
+  uppercase,
+  capitalize,
+  date,
+  number,
+  round,
+  percentage,
+  time_ago,
+  on_empty,
+  translate,
+  week_date,
+  prettify,
+  json,
+  truncate,
 };
+
+export default filters;
--- src/filters/index.js
+++ src/filters/index.js
@@ -1,8 +1,2 @@
-import filters from "./filters.js";
-
-export default function(app, userFilters) {
-  // In Vue 3, filters are removed. We'll expose them as global properties instead.
-  // Users will need to call them as methods: {{ $filters.filterName(value) }}
-  const allFilters = { ...filters, ...userFilters };
-  app.config.globalProperties.$filters = allFilters;
-}
+export * from "./filters.js";
+export { default } from "./filters.js";
